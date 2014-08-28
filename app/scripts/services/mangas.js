c7ae90f7b2d77fa55d38b88239447397cd2bd3d6
Cliko <benjamin-garcia@cifacom.com>
2014-08-28 20:52:46 +0200

    Service mangas.js

--- app/scripts/services/mangas.js
+++ app/scripts/services/mangas.js
@@ -1,6 +1,30 @@
 'use strict';
 
 angular.module('projetMangaApp')
-  .service('Mangas', function Mangas() {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-  });
+    .factory('mangas', function ($http) {
+        // Service logic
+        // ...
+
+        var mangas = [];
+
+        // Public API here
+        return {
+            loadUsers:function(){
+                return $http.get('scipts/datas.json').success(function(data){
+                    mangas=data;
+                })
+            },
+            getMangas: function () {
+                return mangas;
+            },
+            getMangaById: function (mangaId) {
+                for (var i = 0; i < mangas.length; i++) {
+                    var manga = mangas[i];
+                    if(manga.title===mangaId){
+                        return user;
+                    }
+                }
+                return {};
+            }
+        };
+    });
