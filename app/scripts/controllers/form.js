758248f4c4e9bb3cc28ba10388901b252c90d9cb
Cliko <benjamin-garcia@cifacom.com>
2014-08-29 11:52:44 +0200

    Update

---
+++ app/scripts/controllers/form.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('projetMangaApp')
+  .controller('formCtrl', function ($scope,mangas) {
+        mangas.loadMangas().success(function () {
+            $scope.mangas=mangas.getMangas();
+        });
+  });
